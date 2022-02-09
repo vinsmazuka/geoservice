e7255a0a229ed8315906d53d0475725f754003f4
vinsmazuka <distortion1988@mail.ru>
2022-02-09 15:25:56 +0900

    доб импорты библиотек

---
+++ geoservice/showmap/core.py
@@ -0,0 +1,12 @@
+import csv
+import operator
+from math import radians, cos, sin, sqrt
+import numpy
+import folium
+from scipy.spatial import KDTree
+from dadata import Dadata
+from datadata_config import configuration
+
+
+
+
