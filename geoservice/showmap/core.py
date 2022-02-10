bec43eecd91d2c7527629445ca89d0122c0af5c3
vinsmazuka <distortion1988@mail.ru>
2022-02-10 12:47:48 +0900

    изм мет create_map(доб обраб исключ)

--- geoservice/showmap/core.py
+++ geoservice/showmap/core.py
@@ -52,39 +52,46 @@ class Mapper:
         параметре экземпляра self.address
         :return: объект-карту класса folium.Map
         """
-        with self.geocoder(self.token, self.secret) as dadata:
-            address_info = dadata.clean(name="address", source=self.address)
-            lat = address_info['geo_lat']
-            lon = address_info['geo_lon']
-            coordinates = [lat, lon]
-            new_map = self.map_creator(location=coordinates,
-                                       zoom_start=13)
-            self.mark_creator(location=coordinates,
-                              popup=address_info['result']).add_to(new_map)
-        if radius is None:
-            pass
+        if not self.address:
+            print('адрес не задан')
         else:
-            cities = {
-                city['city']:
-                    {'geo_lat': city['geo_lat'],
-                     'geo_lon': city['geo_lon'],
-                     'ecef': Transform.geodetic2ecef(float(city['geo_lat']),
-                                                     float(city['geo_lon']))} for city in cities
-            }
-            ecef_cities = []
-            for key, value in cities.items():
-                ecef_cities.append((key, value['ecef']))
-            tree = KDTree(numpy.array(list(map(lambda x: x[1], ecef_cities))))
-            central_point = Transform.geodetic2ecef(float(coordinates[0]),
-                                                    float(coordinates[1]))
-            result = tree.query_ball_point([central_point], r=radius)
-            cities_around = [x[0] for x in operator.itemgetter(*result[0])(ecef_cities)]
-            for key, value in cities.items():
-                if key in cities_around and key != address_info['city']:
-                    self.mark_creator(location=[value['geo_lat'], value['geo_lon']],
-                                      popup=key,
-                                      icon=self.icon_creator(color='gray')).add_to(new_map)
-        return new_map
+            try:
+                with self.geocoder(self.token, self.secret) as dadata:
+                    address_info = dadata.clean(name="address", source=self.address)
+                    lat = address_info['geo_lat']
+                    lon = address_info['geo_lon']
+                    coordinates = [lat, lon]
+                    new_map = self.map_creator(location=coordinates,
+                                               zoom_start=13)
+                    self.mark_creator(location=coordinates,
+                                      popup=address_info['result']).add_to(new_map)
+            except ValueError:
+                print('адрес не корректный')
+            else:
+                if radius is None:
+                    pass
+                else:
+                    cities = {
+                        city['city']:
+                            {'geo_lat': city['geo_lat'],
+                             'geo_lon': city['geo_lon'],
+                             'ecef': Transform.geodetic2ecef(float(city['geo_lat']),
+                                                             float(city['geo_lon']))} for city in cities
+                    }
+                    ecef_cities = []
+                    for key, value in cities.items():
+                        ecef_cities.append((key, value['ecef']))
+                    tree = KDTree(numpy.array(list(map(lambda x: x[1], ecef_cities))))
+                    central_point = Transform.geodetic2ecef(float(coordinates[0]),
+                                                            float(coordinates[1]))
+                    result = tree.query_ball_point([central_point], r=radius)
+                    cities_around = [x[0] for x in operator.itemgetter(*result[0])(ecef_cities)]
+                    for key, value in cities.items():
+                        if key in cities_around and key != address_info['city']:
+                            self.mark_creator(location=[value['geo_lat'], value['geo_lon']],
+                                              popup=key,
+                                              icon=self.icon_creator(color='gray')).add_to(new_map)
+                    return new_map
 
 
 class Transform:
@@ -127,9 +134,10 @@ class Transform:
 
 
 if __name__ == '__main__':
-    adr = 'Хабаровск Вороилова 3'
+    adr = 'Норильск'
     new_map = Mapper(adr).create_map(cities=CsvReader.read_file('city.csv'),
                                      radius=Transform.euclidean_distance(400))
+    # new_map = Mapper(adr).create_map(cities=CsvReader.read_file('city.csv'))
     new_map.save('new_map.html')
 
 
