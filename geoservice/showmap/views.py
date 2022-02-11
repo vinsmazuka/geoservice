de5d03dcbc095bcfae93973ea29ab18174bd80f6
vinsmazuka <distortion1988@mail.ru>
2022-02-11 14:04:11 +0900

    изм функ index

--- geoservice/showmap/views.py
+++ geoservice/showmap/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from . import core
 
 
 def index(request):
-    return render(request, 'showmap/index.html')
+    args = dict()
+    result = (core.Mapper('Хабароdfgfвск').\
+        create_map(cities=core.CsvReader.read_file('D:\Программирование\Мои программы\кейс на стажера Fogstream\geoservice\geoservice\showmap\city.csv'),
+                   radius=core.Transform.euclidean_distance('')))
+    if result is str:
+        args['resp'] = result
+    else:
+        args['resp'] = result._repr_html_()
+    return render(request, 'showmap/index.html', args)
