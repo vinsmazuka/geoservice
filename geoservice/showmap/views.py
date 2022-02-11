35121482d8d29aa12a98114f9a37366036c2b508
vinsmazuka <distortion1988@mail.ru>
2022-02-11 16:25:22 +0900

    изм ф index

--- geoservice/showmap/views.py
+++ geoservice/showmap/views.py
@@ -4,10 +4,10 @@ from . import core
 
 def index(request):
     args = dict()
-    result = (core.Mapper('Хабароdfgfвск').\
+    result = (core.Mapper('Киров').
         create_map(cities=core.CsvReader.read_file('D:\Программирование\Мои программы\кейс на стажера Fogstream\geoservice\geoservice\showmap\city.csv'),
-                   radius=core.Transform.euclidean_distance('')))
-    if result is str:
+                   radius=core.Transform.euclidean_distance('400')))
+    if isinstance(result, str):
         args['resp'] = result
     else:
         args['resp'] = result._repr_html_()
