bab8cd02e6a966eda0cb410656dbf7b00ace01d1
vinsmazuka <distortion1988@mail.ru>
2022-02-11 19:58:48 +0900

    изм мет index

--- geoservice/showmap/views.py
+++ geoservice/showmap/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from . import core
+from .forms import UserForm
 
 
 def index(request):
-    args = dict()
-    result = (core.Mapper('Киров').
-        create_map(cities=core.CsvReader.read_file('D:\Программирование\Мои программы\кейс на стажера Fogstream\geoservice\geoservice\showmap\city.csv'),
-                   radius=core.Transform.euclidean_distance('400')))
-    if isinstance(result, str):
-        args['resp'] = result
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            address = form.cleaned_data.get("address")
+            radius = form.cleaned_data.get("radius")
+            args = dict()
+            args['form'] = form
+            result = (core.Mapper(address).
+                      create_map(cities=core.CsvReader.read_file(
+                'D:\Программирование\Мои программы\кейс на стажера Fogstream\geoservice\geoservice\showmap\city.csv'),
+                                 radius=core.Transform.euclidean_distance(radius)))
+            if isinstance(result, str):
+                args['resp'] = result
+            else:
+                args['resp'] = result._repr_html_()
     else:
-        args['resp'] = result._repr_html_()
+        args = dict()
+        form = UserForm()
+        args['form'] = form
     return render(request, 'showmap/index.html', args)
