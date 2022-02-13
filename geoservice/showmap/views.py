f85a9675ab6c272421e8e972ac228ebc6cd50ee2
vinsmazuka <distortion1988@mail.ru>
2022-02-13 22:06:52 +0900

    изм функц index

--- geoservice/showmap/views.py
+++ geoservice/showmap/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+
 from . import core
 from .forms import UserForm
+from .models import City
 
 
 def index(request):
+    row_data = list(City.objects.all())
+    cities = {city.city: {'geo_lat': city.geo_lat,
+                          'geo_lon': city.geo_lon} for city in row_data}
     args = dict()
     if request.method == 'POST':
         form = UserForm(request.POST)
@@ -12,9 +17,8 @@ def index(request):
             radius = form.cleaned_data.get("radius")
             args['form'] = form
             result = (core.Mapper(address).
-                      create_map(cities=core.CsvReader.read_file(
-                'D:\Программирование\Мои программы\кейс на стажера Fogstream\geoservice\geoservice\showmap\city.csv'),
-                                 radius=core.Transform.euclidean_distance(radius)))
+                      create_map(cities=cities,
+                                 radius=core.transform_radius(radius)))
             if isinstance(result, str):
                 args['resp'] = result
             else:
