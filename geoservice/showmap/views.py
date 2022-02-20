fe0cb0c54387e38218ae51182b0b42fbc2b583e3
vinsmazuka <distortion1988@mail.ru>
2022-02-20 22:43:31 +0900

    изм ф index

--- geoservice/showmap/views.py
+++ geoservice/showmap/views.py
@@ -11,7 +11,9 @@ def index(request):
     cities = {city.city: {'geo_lat': city.geo_lat,
                           'geo_lon': city.geo_lon} for city in row_data}
     args = dict()
-    if request.method == 'POST':
+
+    def post_response():
+        """формирует ответ в случае поступления POST-запроса"""
         form = UserForm(request.POST)
         if form.is_valid():
             address = form.cleaned_data.get("address")
@@ -24,7 +26,15 @@ def index(request):
                 args['resp'] = result
             else:
                 args['resp'] = result._repr_html_()
-    else:
+
+    def get_response():
+        """формирует ответ в случае поступления GET-запроса"""
         form = UserForm()
         args['form'] = form
+
+    possible_responses = {
+        'POST': post_response,
+        'GET': get_response
+    }
+    possible_responses[request.method]()
     return render(request, 'showmap/index.html', args)
